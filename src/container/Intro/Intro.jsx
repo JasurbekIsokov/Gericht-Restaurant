5419ace56e3bfe1d4ea01f5ffb3ce022a480d09d
Jasurbek <jasurisoqov685@gmail.com>
2022-12-11 22:33:31 +0500

    Laurels section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import { meal } from "../../constants";
+import "./Intro.css";
+
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+
+  return (
+    <div className="app__video">
+      <video
+        ref={vidRef}
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              vidRef.current.pause();
+            } else {
+              vidRef.current.play();
+            }
+          }}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
